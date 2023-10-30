49e691b393d93bf49a479cf989f7f9703846542e
Emmanuel <neverkas@proton.me>
2023-10-30 00:35:36 -0300

    Añadido: (example, ff jk) testbench, pulso alto de clock

--- examples/flipflop_jk/tests/flipflop_jk_tb.py
+++ examples/flipflop_jk/tests/flipflop_jk_tb.py
@@ -2,6 +2,7 @@ import cocotb
 
 from cocotb.triggers import Timer
 from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, FallingEdge
 
 # tests
 @cocotb.test()
@@ -31,3 +32,20 @@ async def salida_mantiene_su_estado_con_pulso_bajo_de_reloj(dut):
     i_k=1
     assert dut.o_q.value.integer == 0
     assert dut.o_qn.value.integer == 1
+
+@cocotb.test()
+async def salida_modifica_su_estado_con_pulso_alto_de_reloj(dut):
+    i_clk=0
+    i_j=1
+    i_k=0
+
+    cocotb.start_soon(Clock(dut.i_clk, 1, units="ns").start())
+    #await Timer(10, units='ns')
+    await FallingEdge(dut.i_clk)
+    #await RisingEdge(dut.i_clk)
+
+    assert dut.o_q.value.integer == 1
+    assert dut.o_qn.value.integer == 0
+
+
+
