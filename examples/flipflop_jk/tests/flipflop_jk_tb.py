7bce9d932d6d8441e6dc94c556b36a12fa7bac09
Emmanuel <neverkas@proton.me>
2023-10-30 16:32:39 -0300

    Añadido: (example, ff jk) testbench en cocotb

--- examples/flipflop_jk/tests/flipflop_jk_tb.py
+++ examples/flipflop_jk/tests/flipflop_jk_tb.py
@@ -7,42 +7,54 @@ from cocotb.triggers import RisingEdge, FallingEdge
 # tests
 @cocotb.test()
 async def salida_mantiene_su_estado_con_pulso_bajo_de_reloj(dut):
-    i_clk=0
-    i_j=0
-    i_k=0
+    dut.i_clk.value=0
+    dut.i_j.value=0
+    dut.i_k.value=0
 
+    print("########################")
     await Timer(10, units='ns')
+    print("o_q = ", dut.o_q.value.integer)
     assert dut.o_q.value.integer == 0
     assert dut.o_qn.value.integer == 1
 
     await Timer(10, units='ns')
-    i_j=1
-    i_k=0
+    dut.i_j.value=1
+    dut.i_k.value=0
+    print("o_q = ", dut.o_q.value.integer)
     assert dut.o_q.value.integer == 0
     assert dut.o_qn.value.integer == 1
 
     await Timer(10, units='ns')
-    i_j=0
-    i_k=1
+    dut.i_j.value=0
+    dut.i_k.value=1
+    print("o_q = ", dut.o_q.value.integer)
     assert dut.o_q.value.integer == 0
     assert dut.o_qn.value.integer == 1
 
     await Timer(10, units='ns')
-    i_j=1
-    i_k=1
+    dut.i_j.value=1
+    dut.i_k.value=1
+    print("o_q = ", dut.o_q.value.integer)
     assert dut.o_q.value.integer == 0
     assert dut.o_qn.value.integer == 1
+    print("########################")
 
 @cocotb.test()
-async def salida_modifica_su_estado_con_pulso_alto_de_reloj(dut):
-    i_clk=0
-    i_j=1
-    i_k=0
+async def salida_modificada_con_flancos_de_subida_de_reloj(dut):
+    task = cocotb.start_soon(Clock(dut.i_clk, 1, units="ns").start())
+    task.log.info("Iniciando Clock!")
 
-    cocotb.start_soon(Clock(dut.i_clk, 1, units="ns").start())
-    #await Timer(10, units='ns')
-    await FallingEdge(dut.i_clk)
-    #await RisingEdge(dut.i_clk)
+    dut.i_j.value=1
+    dut.i_k.value=0
+
+    print("########################")
+    print("o_q = ", dut.o_q.value.integer)
+
+    await RisingEdge(dut.i_clk)
+    # await FallingEdge(dut.i_clk)
+
+    print("o_q = ", dut.o_q.value.integer)
+    print("########################")
 
     assert dut.o_q.value.integer == 1
     assert dut.o_qn.value.integer == 0
