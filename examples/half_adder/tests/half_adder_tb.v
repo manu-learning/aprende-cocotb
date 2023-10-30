67b53ab6d5e629192a0a4bb7025c55c07c8aa66e
Emmanuel <neverkas@proton.me>
2023-10-30 01:41:11 -0300

    Añadido: (example, semi sumador) testbench sin cocotb

--- examples/half_adder/tests/half_adder_tb.v
+++ examples/half_adder/tests/half_adder_tb.v
@@ -1,32 +1,49 @@
-module half_adder_tb();
-   reg A, B;
-   wire suma, acarreo;
+// escala de tiempo
+// =================
+//
+// - la sintáxis es de la forma `timescale utime/precision dónde utime >= precision
+`timescale 1ns/1ns
 
-   half_adder dut(A,B,suma,acarreo);
+// - el testbench no tendrá entradas ni salidas,
+// por eso no las declaramos entre los paréntesis del módulo
+module half_adder_tb();
+   reg i_operando_a, i_operando_b;
+   wire o_suma, _acarreo;
+
+   // half_adder dut(i_operando_a,i_operando_b,suma,acarreo);
+   half_adder dut(
+                  .i_operando_a(i_operando_a),
+                  .i_operando_b(i_operando_b),
+                  .o_acarreo(o_acarreo),
+                  .o_suma(o_suma)
+                  );
 
    initial
      begin
-        $dumpfile("views/half_adder.vcd");
-        $dumpvars(1,half_adder_tb);
+        // imprimimos los valores
+        $monitor("i_operando_a=%0d, i_operando_b=%d, o_acarreo=%0d, o_suma=%d", i_operando_a, i_operando_b, o_acarreo, o_suma);
 
         // 00 es el valor de las entradas A y B respectivamente
-        A=1'b0;
-        B=1'b0;
+        i_operando_a=1'b0;
+        i_operando_b=1'b0;
 
         // 01 es el valor de las entradas A,B respectivamente
         #10
-        A=1'b0;
-        B=1'b1;
+        i_operando_a=1'b0;
+        i_operando_b=1'b1;
 
         // 10 es el valor de las entradas A,B respectivamente
         #10
-        A=1'b1;
-        B=1'b0;
+        i_operando_a=1'b1;
+        i_operando_b=1'b0;
 
         // 11 es el valor de las entradas A,B respectivamente
         #10
-        A=1'b1;
-        B=1'b1;
+        i_operando_a=1'b1;
+        i_operando_b=1'b1;
+
+        #10
+        $finish;
      end
 
 endmodule
